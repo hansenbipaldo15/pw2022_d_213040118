e819402531b7bdca833df49597c2a2bf95725471
hansenbipaldo15 <hansensorga@gmail.com>
2022-02-24 10:53:16 +0700

    mengerjakan tugas pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,68 @@
+<?php
+// Pertemuan 2, belaja mengenal sintaks PHP
+// echo untuk mencetak nilai ke layar
+
+echo 1 + 2 * 3 / 4;
+echo "<hr>";
+
+
+//OPERATOR
+// Aritmatika
+// +, -, *, /, %
+
+echo 5 % 2; // sisa bagi
+echo "<hr>";
+echo 10 + "10" + 10;
+echo '<br>';
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+
+echo 10 != 20;
+echo "<hr>";
+
+// Variabel
+// Tempat menampung nilai
+// tidak boleh mengandung spasi
+// boleh mengandung angka, tapi tidak boleh diawal
+// snake_case : $nama_depan_mahasiswa
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+
+$nama = 'Hansen';
+echo $nama;
+echo "<hr>";
+$x = 1;
+$y = 2;
+
+
+// Penugasan / Assigment
+// =, +=, -=, *=, /=, %=
+$a = 10;
+$a += 20;
+$a += 30;
+echo $a;
+echo "<hr>";
+
+// Increment & Decrement
+// ++, --
+$b = 10;
+echo ++$b;
+echo "<hr>";
+echo ++$b;
+echo --$b;
+echo "<hr>";
+
+// Identitas 
+// ===, !==
+echo 10 === "10";
+?>
+
+
+`back tick
+~ tilde
+# pound
+^ caret
+| pipe
+\ back slash
+/ slash
